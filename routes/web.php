ae74f37cefab2d558101e64f43490303964a039c
kiianh0204200 <anhhdkph30121@fpt.edu.vn>
2024-07-22 02:33:21 +0700

    product-category

--- routes/web.php
+++ routes/web.php
@@ -135,170 +135,23 @@ Route::group(['prefix' => 'admin'], function () {
 
         });
 
-        Route::group(['prefix' => 'role'], function () {
-            Route::get('/', [RoleController::class, 'index'])->name('admin.role.index')->middleware(['permission:read role management']);
-            Route::get('/create', [RoleController::class, 'create'])->name('admin.role.create')->middleware(['permission:create role management']);
-            Route::post('/', [RoleController::class, 'store'])->name('admin.role.store');
-            Route::get('/edit/{id}', [RoleController::class, 'edit'])->name('admin.role.edit')->middleware(['permission:update role management']);
-            Route::patch('/{id}', [RoleController::class, 'update'])->name('admin.role.update');
-            Route::get('/{id}', [RoleController::class, 'destroy'])->name('admin.role.destroy')->middleware(['permission:delete role management']);
-        });
-
-        Route::group(['prefix' => 'product-option'], function () {
-            Route::get('/', [ProductOptionController::class, 'index'])->name('admin.product-option.index')->middleware(['permission:read product option management']);
-            Route::get('/create', [ProductOptionController::class, 'create'])->name('admin.product-option.create')->middleware(['permission:create product option management']);
-            Route::post('/', [ProductOptionController::class, 'store'])->name('admin.product-option.store');
-            Route::get('/edit/{id}', [ProductOptionController::class, 'edit'])->name('admin.product-option.edit')->middleware(['permission:update product option management']);
-            Route::patch('/{id}', [ProductOptionController::class, 'update'])->name('admin.product-option.update');
-            Route::get('/{id}', [ProductOptionController::class, 'destroy'])->name('admin.product-option.destroy')->middleware(['permission:delete product option management']);
-        });
-
-        Route::group(['prefix' => 'order'], function () {
-            Route::get('/', [OrderController::class, 'index'])->name('admin.order.index')->middleware(['permission:read order management']);
-            Route::get('/show/{id}', [OrderController::class, 'show'])->name('admin.order.show')->middleware(['permission:read order management']);
-            Route::get('/edit/{id}', [OrderController::class, 'edit'])->name('admin.order.edit')->middleware(['permission:update order management']);
-            Route::patch('/{id}', [OrderController::class, 'update'])->name('admin.order.update');
-            Route::get('/{id}', [OrderController::class, 'destroy'])->name('admin.order.destroy')->middleware(['permission:delete order management']);
-        });
-
-        Route::group(['prefix' => 'contact'], function () {
-            Route::get('/', [ContactController::class, 'index'])->name('admin.contact.index')->middleware(['permission:read contact management']);
-            Route::get('/show/{id}', [ContactController::class, 'show'])->name('admin.contact.show')->middleware(['permission:read contact management']);
-            Route::get('/{id}', [ContactController::class, 'destroy'])->name('admin.contact.destroy')->middleware(['permission:delete contact management']);
-        });
-
-        Route::group(['prefix' => 'users'], function () {
-            Route::get('/', [UserController::class, 'index'])->name('admin.users.index')->middleware(['permission:read user management']);
-            Route::get('/view/{id}', [UserController::class, 'show'])->name('admin.users.show')->middleware(['permission:read user management']);
-            Route::get('/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy')->middleware(['permission:delete user management']);
-        });
-    });
-});
-
+ 
 // Frontend Routes
-Route::get('/', [HomeController::class, 'index'])->name('frontend.home');
-Route::get('/order-track', [HomeController::class, 'orderTrack'])->name('order.track');
-Route::post('product/track/order', [HomeController::class, 'productTrackOrder'])->name('product.track.order');
 
 
-Route::get('/about-us', [AboutController::class, 'index']);
 
-Route::get('/contact', [ContactController::class, 'createForm']);
-Route::post('/contact', [ContactController::class, 'ContactUsForm'])->name('contact.store');
 
-Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 
-Route::get('/blog-detail/{id}', [BlogDetailController::class, 'index'])->name('blog.detail');
 
-Route::post('product-comment', [ProductCommentController::class, 'store'])->name('product.comment.store');
 
-Route::get('/login', [FrontendLoginController::class, 'index']);
-Route::post('/login', [FrontendLoginController::class, 'login'])->name('frontend.login');
-Route::get('/logout', [FrontendLoginController::class, 'logout'])->name('frontend.logout');
 
-Route::get('/register', [FrontendRegisterController::class, 'index']);
-Route::post('/register', [FrontendRegisterController::class, 'register'])->name('frontend.register');
 
-Route::get('/product-detail/{id}', [ProductDetailsController::class, 'index'])->name('product.detail');
-Route::get('/product-detail-size', [ProductDetailsController::class, 'getSize'])->name('product.detail.size');
-Route::get('/product-detail-size-price', [ProductDetailsController::class, 'getPriceBySize'])->name('product.detail.size.price');
 
-Route::get('/shop', [ProductListController::class, 'index'])->name('shop');
 
-Route::group(['middleware' => ['auth:web', 'verified']], function () {
-    Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
-    Route::post('/checkout', [CheckoutController::class, 'store'])->name('frontend.checkout.store');
 
-    Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 
-    Route::prefix('/my-account')->group(function () {
-        Route::get('/', [AccountController::class, 'index'])->name('frontend.user.index');
-        Route::patch('/{id}', [AccountController::class, 'update'])->name('frontend.user.update');
-        Route::get('/order-detail/{id}', [AccountController::class, 'orderDetail'])->name('frontend.user.order-detail');
-        Route::get('/order-cancel/{id}', [AccountController::class, 'orderCancel'])->name('frontend.user.order-cancel');
-    });
-});
-
-Route::get('/payment-return', [CheckoutController::class, 'paymentReturn'])->name('frontend.checkout.vnpay');
-
-Route::get('/cart', [CartController::class, 'index'])->middleware('auth:web')->name('cart');
-Route::post('/cart/add', [CartController::class, 'store'])->name('cart.add');
-Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
-Route::get('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
-Route::get('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
-
-Route::get('/wishlist', [WishlistController::class, 'index']);
-
-Route::get('/privacy-policy', [PrivacyPolicyController::class, 'index']);
-
-Route::get('/terms', [TermController::class, 'index']);
-
-Route::get('/email/verify', function () {
-    return view('frontend.pages.email-verify');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    toastr()->success(__('frontend.Email verified successfully!'));
-    return redirect('/');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    toastr()->success(__('frontend.Email verification link sent successfully!'));
-    return back();
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 // Password Reset Routes...
-Route::get('/forgot-password', function () {
-    return view('frontend.pages.forgot-password');
-})->middleware('guest')->name('password.request');
 
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
-
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
-
-    toastr()->success(__('frontend.Reset password link sent successfully!'));
-    return $status === Password::RESET_LINK_SENT
-        ? back()->with(['status' => __($status)])
-        : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
-
-Route::get('/reset-password/{token}', function ($token) {
-    return view('frontend.pages.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', function (Request $request) {
-
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:8|confirmed',
-    ]);
-
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function (User $user, string $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
-
-            $user->save();
-
-            event(new PasswordReset($user));
-        }
-    );
-
-    toastr()->success(__('frontend.Password reset successfully!'));
-
-    return $status === Password::PASSWORD_RESET
-        ? redirect()->route('frontend.login')->with(['status', __($status)])
-        : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password.update');
 
 // End frontend routes
