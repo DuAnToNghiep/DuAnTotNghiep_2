b6933d5c848bbf3a7d11d310262faffaf8a7595d
binh le <accfreefire2504@gmail.com>
2024-08-12 00:02:04 +0700

    Privacy update

--- app/Http/Controllers/Frontend/PrivacyPolicyController.php
+++ app/Http/Controllers/Frontend/PrivacyPolicyController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class PrivacyPolicyController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('frontend.pages.privacy-policy');
+    // }
     public function index()
     {
         return view('frontend.pages.privacy-policy');
