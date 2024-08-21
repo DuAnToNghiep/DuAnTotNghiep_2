e290600f9a588eaec3f6ef731480609604c41ac4
binh le <accfreefire2504@gmail.com>
2024-08-22 01:37:18 +0700

    edit

--- resources/views/backend/role/edit.blade.php
+++ resources/views/backend/role/edit.blade.php
@@ -42,7 +42,8 @@
                             @endforeach
 
                             @error('name')
-                            <div class="invalid-feedback">{{ $message }}</div>
+                            {{-- <div class="invalid-feedback">{{ $message }}</div> --}}
+                            {{-- <div class="invalid-feedback">{{ $message }}</div> --}}
                             @enderror
                         </div>
                     </div>
