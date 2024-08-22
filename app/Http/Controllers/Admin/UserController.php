db471d4b04fb3db8098ec9478b8207b0922c1135
binh le <accfreefire2504@gmail.com>
2024-08-22 22:32:57 +0700

    update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
      */
     public function index(UserDataTable $userDataTable)
     {
-        return $userDataTable->render('backend.user.index');
+        // return $userDataTable->render('backend.user.index');
     }
 
     /**
